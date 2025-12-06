8f01b69f598f1bb5c1973617894004287353b088
shenlvcheng <shenlc@staryea.com>
2025-12-06 18:55:46 +0800

    fix: 用反引号包裹 ClickHouse 数据库名避免特殊字符导致语法错误

--- backend/modules/evaluation/infra/repo/experiment/ck/expt_turn_result_filter.go
+++ backend/modules/evaluation/infra/repo/experiment/ck/expt_turn_result_filter.go
@@ -523,9 +523,9 @@ func (d *exptTurnResultFilterDAOImpl) buildKeywordSearchConditions(ctx context.C
 
 // buildBaseSQL 构建基础SQL语句
 func (d *exptTurnResultFilterDAOImpl) buildBaseSQL(ctx context.Context, joinSQL, whereSQL, keywordCond, evalSetSyncCkDate string, args *[]interface{}) string {
-	sql := "SELECT  etrf.item_id, etrf.status FROM " + d.configer.GetCKDBName(ctx).ExptTurnResultFilterDBName + ".expt_turn_result_filter etrf"
+	sql := "SELECT  etrf.item_id, etrf.status FROM `" + d.configer.GetCKDBName(ctx).ExptTurnResultFilterDBName + "`.expt_turn_result_filter etrf"
 	if joinSQL != "" || keywordCond != "" {
-		sql += " INNER JOIN " + d.configer.GetCKDBName(ctx).DatasetItemsSnapshotDBName + ".dataset_item_snapshot dis ON etrf.eval_set_version_id = dis.version_id AND etrf.item_id = dis.item_id"
+		sql += " INNER JOIN `" + d.configer.GetCKDBName(ctx).DatasetItemsSnapshotDBName + "`.dataset_item_snapshot dis ON etrf.eval_set_version_id = dis.version_id AND etrf.item_id = dis.item_id"
 	}
 
 	sql += " WHERE 1=1"
@@ -672,7 +672,7 @@ func (d *exptTurnResultFilterDAOImpl) buildGetByExptIDItemIDsSQL(ctx context.Con
 		"etrf.evaluator_score['key9'] as evaluator_score_key_9, " +
 		"etrf.evaluator_score['key10'] as evaluator_score_key_10, " +
 		"etrf.evaluator_score_corrected " +
-		"FROM " + d.configer.GetCKDBName(ctx).ExptTurnResultFilterDBName + ".expt_turn_result_filter etrf " +
+		"FROM `" + d.configer.GetCKDBName(ctx).ExptTurnResultFilterDBName + "`.expt_turn_result_filter etrf " +
 		"WHERE etrf.space_id = ? AND etrf.expt_id = ? AND etrf.created_date =?"
 	if len(itemIDs) > 0 {
 		sql += " AND etrf.item_id IN (?)"
